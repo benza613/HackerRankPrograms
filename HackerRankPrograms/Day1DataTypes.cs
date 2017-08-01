df0f7393eeb0e4948074fef69f272d352fc03218
benza613 <benito.alvares@gmail.com>
2017-08-01 14:45:41 +0530

    Day1 code data types

---
+++ HackerRankPrograms/Day1DataTypes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankPrograms
+{
+    class Day1DataTypes
+    {
+        static void Main(String[] args)
+        {
+            int i = 4;
+            double d = 4.0;
+            string s = "HackerRank ";
+
+            void c(string st)
+            {
+                Console.WriteLine(st);
+            }
+
+            int ri = i + Convert.ToInt32(Console.ReadLine());
+            string rds = Console.ReadLine();
+            decimal rd = (decimal)d + Convert.ToDecimal(rds);
+
+            string rs = s + Console.ReadLine();
+
+            c(ri.ToString());
+            c(rd.ToString("#.0"));
+            c(rs.ToString());
+
+            var x = Console.ReadLine();
+
+            
+        }
+
+        
+    }
+}
--- HackerRankPrograms/Program.cs
+++ HackerRankPrograms/Program.cs
@@ -4,16 +4,12 @@ namespace HackerRankPrograms
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
             string[] u = Console.ReadLine().Split(' ');
             int[] unsortedInt = Array.ConvertAll(u, Int32.Parse);
-
-            
-
             float[] r = new Stats10days0().Result(unsortedInt);
-
             c(r[0].ToString("0.0"));
             c(r[1].ToString());
             c(r[2].ToString());
