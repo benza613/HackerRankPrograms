6c1f9fc65bf90c553b8996a8def9413b6011509e
benza613 <benito.alvares@gmail.com>
2017-08-02 12:52:04 +0530

    Solved day 2 operators

---
+++ HackerRankPrograms/Day2Operators.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankPrograms
+{
+    class Day2Operators
+    {
+        static void Main5(String[] args)
+        {
+            decimal mealCost = decimal.Round(Convert.ToDecimal(Console.ReadLine()),2,MidpointRounding.ToEven);
+            int tipPercent = Convert.ToInt32(Console.ReadLine());
+            int taxPercent = Convert.ToInt32(Console.ReadLine());
+
+            decimal tip = mealCost *( tipPercent / (decimal)100);
+            decimal tax = mealCost *( taxPercent / (decimal)100);
+            decimal totalCost = mealCost + tip + tax;
+
+            Console.WriteLine("The total meal cost is "+ Convert.ToInt32(totalCost) + " dollars.");
+        }
+    }
+}
