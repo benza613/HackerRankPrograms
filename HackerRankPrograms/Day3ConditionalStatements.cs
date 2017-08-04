ad41fda6ada5156f4900be539a79ad5673419f25
benza613 <benito.alvares@gmail.com>
2017-08-04 12:24:10 +0530

    solved day 3 challenge

---
+++ HackerRankPrograms/Day3ConditionalStatements.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankPrograms
+{
+    class Day3ConditionalStatements
+    {
+        static void Main6(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string result = "";
+            if (n % 2 == 1)
+            {
+                result = "Weird";
+            }
+            else
+            {
+                if(n >= 2 && n <= 5)
+                {
+                    result = "Not Weird";
+                }
+                else if(n >= 6 && n <= 20)
+                {
+                    result = "Weird";
+                }
+                else
+                {
+                    result = "Not Weird";
+                }
+            }
+
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
