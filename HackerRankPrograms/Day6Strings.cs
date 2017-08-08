c413fddc725a3d41f4d1acbfe3240db0a59ea316
benza613 <benito.alvares@gmail.com>
2017-08-08 12:55:49 +0530

    Solved day 6 of 30 days of code : lets review challenge

---
+++ HackerRankPrograms/Day6Strings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankPrograms
+{
+    class Day6Strings
+    {
+        static void Main2(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                char[] arr1 = arr[i].ToCharArray();
+                char[] res = new char[arr1.Length + 1];
+                int counter = 0;
+                for (int j = 0; j < arr1.Length; j = j + 2)
+                {
+                    res[counter] = arr1[j];
+                    counter++;
+                }
+
+                res[counter] = ' ';
+                counter++;
+
+                for (int j = 1; j < arr1.Length; j = j + 2)
+                {
+                    res[counter] = arr1[j];
+                    counter++;
+                }
+
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
---
+++ HackerRankPrograms/Quartiles.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankPrograms
+{
+    class Quartiles
+    {
+        //solution pending
+        static void Main3(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] u = Console.ReadLine().Split(' ');
+            int[] x = Array.ConvertAll(u, Int32.Parse);
+
+            // we sorted the array from low to high
+            Array.Sort(x);
+
+            int[] res = new int[3];
+            int upper = 0;
+            int lower = 0;
+
+            int[] up;
+            int[] low;
+
+            //if even number of elements
+            if (x.Length % 2 == 0)
+            {
+                res[1] = (x[x.Length / 2] + x[(x.Length / 2) - 1]) / 2;
+                lower = (x.Length / 2) - 1;
+                upper = x.Length / 2;
+
+            }
+            else
+            {
+                res[1] = x[x.Length / 2];
+                lower = (x.Length / 2) - 1;
+                upper = (x.Length / 2) + 1;
+            }
+
+            Console.WriteLine(res[1]);
+
+            low = new int[lower + 1];
+            up = new int[n - upper];
+
+            for(int i= 0; i <= lower; i++)
+            {
+                low[i] = x[i];
+                up[i] = x[i + upper];
+            }
+
+
+
+
+            //now check for lower is even or odd 
+            if (low.Length % 2 == 0)
+            {
+                res[0] = (low[low.Length / 2] + x[(low.Length / 2) - 1]) / 2;
+            }
+            else
+            {
+                res[0] = x[low.Length / 2];
+            }
+
+            //now check for upper is even or odd 
+            if (up.Length % 2 == 0)
+            {
+                res[2] = (x[up.Length / 2] + x[(up.Length / 2) - 1]) / 2;
+            }
+            else
+            {
+                res[2] = x[up.Length / 2];
+            }
+
+            Console.WriteLine(res[0]);
+            Console.WriteLine(res[1]);
+            Console.WriteLine(res[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
